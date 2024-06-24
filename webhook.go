3fd6f7322144ad80e4b0d3fa85af4e25f2dceac2
maura fortino <maurafortino@gmail.com>
2024-06-24 10:53:16 -0400

    added validation functions structure for v2

--- webhook.go
+++ webhook.go
@@ -305,7 +305,7 @@ func (v1 *RegistrationV1) ValidateDuration(ttl time.Duration) error {
 func (v1 *RegistrationV1) ValidateFailureURL(c *urlegit.Checker) error {
 	if v1.FailureURL != "" {
 		if err := c.Text(v1.FailureURL); err != nil {
-			return fmt.Errorf("%w: failure url is invalid", ErrInvalidInput)
+			return fmt.Errorf("%w: failure url is invalid", err)
 		}
 	}
 	return nil
@@ -362,4 +362,137 @@ func (v1 *RegistrationV1) ValidateUntil(jitter time.Duration, maxTTL time.Durati
 
 func (v1 *RegistrationV1) SetNowFunc(now func() time.Time) {
 	v1.nowFunc = now
+}
+
+func (v2 *RegistrationV2) ValidateOneEvent() error {
+	// if len(v2.) == 0 {
+	// 	return fmt.Errorf("%w: cannot have zero events", ErrInvalidInput)
+	// }
+	return nil
+}
+
+func (v2 *RegistrationV2) ValidateEventRegex() error {
+	// var errs error
+	// for _, e := range v1.Events {
+	// 	_, err := regexp.Compile(e)
+	// 	if err != nil {
+	// 		errs = errors.Join(errs, fmt.Errorf("%w: unable to compile matching", ErrInvalidInput))
+	// 	}
+	// }
+	return nil
+}
+
+func (v2 *RegistrationV2) ValidateDeviceId() error {
+	// var errs error
+	// for _, e := range v2.Matcher {
+	// 	_, err := regexp.Compile(e)
+	// 	if err != nil {
+	// 		errs = errors.Join(errs, fmt.Errorf("%w: unable to compile matching", ErrInvalidInput))
+	// 	}
+	// }
+	return nil
+}
+
+func (v2 *RegistrationV2) ValidateFailureURL(c *urlegit.Checker) error {
+	if v2.FailureURL != "" {
+		if err := c.Text(v2.FailureURL); err != nil {
+			return fmt.Errorf("%w: failure url is invalid", err)
+		}
+	}
+	return nil
+}
+
+func (v2 *RegistrationV2) ValidateReceiverURL(c *urlegit.Checker) error {
+	// if v2.Config.ReceiverURL != "" {
+	// 	if err := c.Text(v1.Config.ReceiverURL); err != nil {
+	// 		return fmt.Errorf("%w: receiver url is invalid", ErrInvalidInput)
+	// 	}
+	// }
+	return nil
+}
+
+func (v2 *RegistrationV2) ValidateAltURL(c *urlegit.Checker) error {
+	// var errs error
+	// for _, webhook := range v2.Webhooks{
+	// 	for _, url := range webhook.ReceiverURLs {
+	// 		if err := c.Text(url); err != nil {
+	// 			errs = errors.Join(errs, fmt.Errorf("%w: url is invalid", ErrInvalidInput))
+	// 		}
+	// 	}
+	// }
+
+	// return errs
+	return nil
+}
+
+func (v2 *RegistrationV2) SetNowFunc(now func() time.Time) {
+
+}
+
+func (v2 *RegistrationV2) ValidateDuration(ttl time.Duration) error {
+	// var errs error
+	// if ttl <= 0 {
+	// 	ttl = time.Duration(0)
+	// }
+
+	// if ttl != 0 && ttl < time.Duration(v1.Duration) {
+	// 	errs = errors.Join(errs, fmt.Errorf("%w: the registration is for too long", ErrInvalidInput))
+	// }
+
+	// if v1.Until.IsZero() && v1.Duration == 0 {
+	// 	errs = errors.Join(errs, fmt.Errorf("%w: either Duration or Until must be set", ErrInvalidInput))
+	// }
+
+	// if !v1.Until.IsZero() && v1.Duration != 0 {
+	// 	errs = errors.Join(errs, fmt.Errorf("%w: only one of Duration or Until may be set", ErrInvalidInput))
+	// }
+
+	// if !v1.Until.IsZero() {
+	// 	nowFunc := time.Now
+	// 	if v1.nowFunc != nil {
+	// 		nowFunc = v1.nowFunc
+	// 	}
+
+	// 	now := nowFunc()
+	// 	if ttl != 0 && v1.Until.After(now.Add(ttl)) {
+	// 		errs = errors.Join(errs, fmt.Errorf("%w: the registration is for too long", ErrInvalidInput))
+	// 	}
+
+	// 	if v1.Until.Before(now) {
+	// 		errs = errors.Join(errs, fmt.Errorf("%w: the registration has already expired", ErrInvalidInput))
+	// 	}
+	// }
+
+	// return errs
+	return nil
+}
+
+func (v2 *RegistrationV2) ValidateNoUntil() error {
+	// if !v1.Until.IsZero() {
+	// 	return fmt.Errorf("%w: Until is not allowed", ErrInvalidInput)
+	// }
+	return nil
+}
+
+func (v2 *RegistrationV2) ValidateUntil(jitter time.Duration, maxTTL time.Duration, now func() time.Time) error {
+	// if now == nil {
+	// 	now = time.Now
+	// }
+	// if maxTTL < 0 {
+	// 	return ErrInvalidInput
+	// } else if jitter < 0 {
+	// 	return ErrInvalidInput
+	// }
+
+	// if v1.Until.IsZero() {
+	// 	return nil
+	// }
+	// limit := (now().Add(maxTTL)).Add(jitter)
+	// proposed := (v1.Until)
+	// if proposed.After(limit) {
+	// 	return fmt.Errorf("%w: %v after %v",
+	// 		ErrInvalidInput, proposed.String(), limit.String())
+	// }
+	return nil
+
 }
